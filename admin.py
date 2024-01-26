8074c1d7cedf1580f018543b58dadb6109be8f38
Verb_ <jeremy.beaucage@uvm.edu>
2024-01-26 16:14:15 -0500

    Some suedocode

--- admin.py
+++ admin.py
@@ -1,1 +1,9 @@
 # Chase Will work on this
+
+#Delete all data that may exist in firestore
+
+#Get json file name from user with validation
+    #call Load data(Json)
+
+#Load data(JSON) Fills our firestore database
+

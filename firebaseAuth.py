40fa24038be5b537d0cb0e5862e8bd206c34a0d5
Nathaniel Borrok-Hoffman <nathaniel.borrok-hoffman@uvm.edu>
2024-02-03 19:00:09 -0500

    added service-account

--- firebaseAuth.py
+++ firebaseAuth.py
@@ -1,7 +1,12 @@
 
 import firebase_admin
+from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
+
+cred = credentials.Certificate("movies-database-f449c-firebase-adminsdk-rh05s-7d539e0132.json")
+app = firebase_admin.initialize_app(cred)
+
 db = firestore.client()
+
+print(cred)

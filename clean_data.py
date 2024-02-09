ae6c786204169250b3cf8bc228936967a9e8dc83
MurphyPeisel <mkpeisel@gmail.com>
2024-02-09 12:55:07 -0500

    edit data

--- clean_data.py
+++ clean_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv("imdb_top_49_edited.csv")
 df["runtime"] = df["runtime"].str.replace(" min","")
-df["gross"] = df["gross"].str.replace(",","") 
+df["gross"] = df["gross"].str.replace(",","")
+df = df.drop(columns=["certificate"])
 
-
-df.to_csv("test.csv")
+df.to_csv("imdb_edited.csv")
